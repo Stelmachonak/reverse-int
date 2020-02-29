3c7b2ba3d7665f9ccd497d034ba6eef09f127efc
Stelmachonak <362060@mail.ru>
2020-02-29 17:40:24 +0300

    feat: add solution for task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = Math.abs(n);
+    }
+    let strN = n.toString();
+    let result = strN
+        .split("")
+        .reverse()
+        .join("");
+    return result;
+};
+
